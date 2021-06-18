9e957ae545a4c067c7997de1f8bc6ceb1b993613
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-06-18 15:21:00 +0200

    completed chat basic UI

--- src/app/main/chat/chat-content/chat-content.component.ts
+++ src/app/main/chat/chat-content/chat-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 
 @Component({
@@ -7,11 +7,28 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./chat-content.component.scss']
 })
 export class ChatContent implements OnInit{
+    @Input() conversation; 
 
     constructor(){}
 
     ngOnInit(): void {
     }
 
+    onSubmitMessage(event){
+        let value = event.target.value.trim(); //save the input value
+        event.target.value = '';
+        if(value.length < 1){ //non permetto l'invio di messaggi vuoti
+            return;
+        }
+        //aggiungo il msg
+        this.conversation.latestMassege = value;
+        this.conversation.messages.unshift({
+            id: 1,
+            body: value,
+            time: '8:23',
+            me: true
+        });
+
+    }
     
 }
--- src/app/main/chat/sidebar/sidebar.component.ts
+++ src/app/main/chat/sidebar/sidebar.component.ts
@@ -8,23 +8,103 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 export class SidebarComponent implements OnInit {
     @Output() conversationClicked: EventEmitter<any> = new EventEmitter();
 
-    conversations: {name: string, time: string, latestMassege: string, id: number}[] = [
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 }
+    conversations = [
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 ,messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ]},
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ]},
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ]},
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] },
+        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
+            {id: 1, body: 'Hello world', time: '8:21', me: true},
+            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
+            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
+            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
+        ] }
     ];
 
     constructor() { }
