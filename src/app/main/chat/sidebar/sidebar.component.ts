4a01a0ed6bda3f1ba4911b6beac2bb96c69d522b
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-06-28 15:53:39 +0200

    add view profile picture in chat

--- src/app/main/chat/sidebar/sidebar.component.ts
+++ src/app/main/chat/sidebar/sidebar.component.ts
@@ -13,8 +13,10 @@ export class SidebarComponent implements OnInit {
     @Output() conversationClicked: EventEmitter<any> = new EventEmitter();
 
     user;
+    userProfilePic;
     conversationsLoaded: boolean = false;
-    conversations = [
+    conversations = [];
+    /*conversations = [
         {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0 ,messages: [
             {id: 1, body: 'Hello world', time: '8:21', me: true},
             {id: 2, body: 'Hello Bro', time: '8:21', me: false},
@@ -26,92 +28,8 @@ export class SidebarComponent implements OnInit {
             {id: 2, body: 'Hello Bro', time: '8:21', me: false},
             {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
             {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ]},
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ]},
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] },
-        {name: "David", time: "8:21 AM", latestMassege: 'ciao capo padrone', id: 0, messages: [
-            {id: 1, body: 'Hello world', time: '8:21', me: true},
-            {id: 2, body: 'Hello Bro', time: '8:21', me: false},
-            {id: 3, body: 'Che cazzo vuoi da me scemo', time: '8:21', me: true},
-            {id: 4, body: 'Hello Bro', time: '8:21', me: true}
-        ] }
-    ];
+        ]}
+    ];*/
 
     constructor(private messageService: MessagesService,
                 private profileService: ProfilesService,
@@ -119,6 +37,9 @@ export class SidebarComponent implements OnInit {
 
     ngOnInit(): void {
         this.user = this.profileService.getProfileLogged();
+        this.profileService.fetchAccount(this.user.id).subscribe(response => {
+            this.userProfilePic = response.proPic;
+        })
 
         this.messageService.getConversations().subscribe(response => {
             console.log(response);
