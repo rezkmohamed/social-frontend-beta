1292b41bc72e6c3e9584b0e1f53518dfa07232bb
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-07-07 10:40:32 +0200

    refactor chat component

--- src/app/main/chat/chat.component.ts
+++ src/app/main/chat/chat.component.ts
@@ -33,16 +33,9 @@ export class ChatComponent implements OnInit, OnDestroy{
             this.messageService.newMessagesForConversation.set(conversation, 0);
             this.messageService.setMessagesAsSeen(conversation.idConversation).subscribe(response => {
                 console.log(response);
-                //this.messageService.newMessagesForConversation.set(this.conversation, 0);
             })
         });
-        /*if(this.user.id === this.conversation.profile1.id){
-            this.user = this.conversation.profile1;
-        } else if(this.user.id === this.conversation.profile2.id){
-            this.user = this.conversation.profile2;
-            this.conversation.profile2 = this.conversation.profile1;
-            this.conversation.profile1 = this.user;
-        }*/
+
         this.messageService.conversation = this.conversation;
     }
 
--- src/app/main/services/messages.service.ts
+++ src/app/main/services/messages.service.ts
@@ -8,7 +8,6 @@ import { MessageModel } from "../models/message.model";
 })
 export class MessagesService {
     private urlBase: string = "http://localhost:8080/";
-    token: string = JSON.parse(localStorage.getItem('userData'))._token.toString();
     conversation;
     conversations: Conversation[];
     newMessagesForConversation: Map<Conversation, number> = new Map();
@@ -22,7 +21,9 @@ export class MessagesService {
 
         this.webSocket.onopen = (event) => {
             console.log('Open: ' + event);
-            this.webSocket.send(JSON.stringify("Bearer " + this.token));
+            let token: string = JSON.parse(localStorage.getItem('userData'))._token.toString();
+
+            this.webSocket.send(JSON.stringify("Bearer " + token));
             this.conversations = [];
         };
 
@@ -33,34 +34,12 @@ export class MessagesService {
             console.log(this.conversations);
             for(let conv of this.conversations){
                 if(conv.idConversation === chatMessageDTO.idConversation){
-                    console.log("conversation found");
-                    console.log(conv);
                     conv.messages.unshift(chatMessageDTO);
                     conv.latestMessage = chatMessageDTO.message;
                     conv.latestMessageDate = chatMessageDTO.date;
                     this.newMessagesForConversation.set(conv, 1);
                 }
             }
-            /*if(this.conversation){
-                if(this.conversation.idConversation === chatMessageDTO.idConversation){
-                    this.conversation.messages.unshift(chatMessageDTO);
-                    this.conversation.latestMessage = chatMessageDTO.message;
-                    this.conversation.latestMessageDate = chatMessageDTO.date;
-                    this.newMessagesForConversation.set(this.conversation, this.newMessagesForConversation.get(this.conversation) +1);
-                    console.log('if pushed');
-                }
-            }
-            else {
-                for(let cnv of this.conversations){
-                    if(cnv.idConversation === chatMessageDTO.idConversation){
-                        cnv.latestMessage = chatMessageDTO.message;
-                        cnv.latestMessageDate = chatMessageDTO.date;
-                        this.newMessagesForConversation.set(cnv, this.newMessagesForConversation.get(cnv) +1);
-                        console.log('else pushed');
-                    }
-                }
-            }
-            return false;*/
         }
 
         this.webSocket.onclose = (event) => {
