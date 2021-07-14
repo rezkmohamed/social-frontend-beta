36f6f421d604da6666dd151196e7d887a8437def
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-07-14 17:45:06 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBase: "https://fakegramtest.herokuapp.com/",
-  wsBase: "ws://fakegramtest.herokuapp.com/"
+  wsBase: "wss://fakegramtest.herokuapp.com/"
 };
